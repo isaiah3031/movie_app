c27de2328b0c84b15a1546a0222f9d78ad2f33da
isaiah3031 <isaiah.whitfield@gmail.com>
2021-01-28 11:15:59 -0500

    Going to remove react files from the api file.

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import $ from "jquery"
-
-window.$ = $
-window.jQuery = $
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
