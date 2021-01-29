e08c19029f303fa8ce3ad6fb5e9c9c7a3d422827
isaiah3031 <isaiah.whitfield@gmail.com>
2021-01-29 10:57:05 -0500

    Working on heroku issues.

--- client/src/frontend/components/app.jsx
+++ client/src/frontend/components/app.jsx
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom'
 import SignupFormContainer from './session/signup_form_container'
 import LoginFormContainer from './session/login_form_container'
 import MovieIndexContainer from './movies/movie_index_container'
-
+//
 const App = () => {
   return (
     <div>
