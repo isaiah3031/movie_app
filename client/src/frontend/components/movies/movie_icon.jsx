cb726fe6c96397b10192c0308bb6c1d3b185475f
isaiah3031 <isaiah.whitfield@gmail.com>
2021-02-01 09:44:22 -0500

    Discovered the node package 'react-multi-carousel' and implemented it instead of the solution I had before. This has saved a ton of time and I'm very happy with the results.

--- client/src/frontend/components/movies/movie_icon.jsx
+++ client/src/frontend/components/movies/movie_icon.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import '../../../stylesheets/small_icons.scss'
 
 function MovieIcon(props) {
-  const {movie: { title, poster_path }} = props
+  const {id, movie: { title, poster_path }} = props
   return (
-    <div className='small_icons'>
+    <div className='small_icons' id={id}>
       <img src={`https://image.tmdb.org/t/p/w500${poster_path}`}/>
     </div>
   )
--- client/src/frontend/components/movies/movies_by_genre.jsx
+++ client/src/frontend/components/movies/movies_by_genre.jsx
@@ -1,59 +1,48 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import MovieIcon from './movie_icon'
 import '../../../stylesheets/horizontal_lists.scss'
 import $ from 'jquery'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css';
+function MoviesByGenre(props) {
+  useEffect(() => {
+    props.fetchMoviesByGenre(props.genre.id)
+  }, [])
 
-class MoviesByGenre extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      listStart = 0
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 8,
+      slidesToSlide: 8
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      slidesToSlide: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+      slidesToSlide: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
     }
-  }
+  };
 
-  componentWillMount() {
-    this.props.fetchMoviesByGenre(this.props.genre.id)
-  }
-
-  changeListStart(direction) {
-    if (direction === 'left' && this.state.listStart != 0) {
-      this.setState({
-        listStart: this.state.listStart -= 5
-      })
-    } else if (direction === 'right' && this.state.listStart != 20) {
-      this.setState({
-        listStart: this.state.listStart += 5
-      })
-    } else {
-      return 'no movement'
-    }
-  }
-  
-  autoScroll(direction) {
-    this.changeListStart(direction)
-    if (direction === 'left') {  
-      $(`#${this.props.genre.id}`).scrollBy(0, 100)
-    } else {
-      $(`#${this.props.genre.id}`).anitmate({
-        scrollTop: $('#2').offset().right
-      }, 2000)
-    }
-  }
-
-  render() {
     try {
       return ( 
-          <div>
-            <button onClick={(e) => this.autoScroll(e)}  id='left'>left</button>        
-            <div className='horizontal-lists' id={this.props.genre.id}>
+          <Carousel 
+            infinite={true}
+            responsive={responsive}>        
             {
-              this.props.movies[this.props.genre.id].map((movie, index) => {
+              props.movies[props.genre.id].map((movie, index) => {
               return <MovieIcon id={index} movie={movie} />
             }
-            )}
-          </div>
-          <button onClick={(e) => this.autoScroll(e.target.id)} id='right'>right</button>
-        </div>
+          )}
+        </Carousel>
       )
     } catch (error) {
       
@@ -61,7 +50,7 @@ class MoviesByGenre extends React.Component {
     return (
       null
     )
-  }
+  
 }
 
 export default MoviesByGenre
