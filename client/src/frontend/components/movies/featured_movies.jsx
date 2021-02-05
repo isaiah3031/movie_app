2ad43bde5f4d394fa3678be37dac2beb1f09acaa
isaiah3031 <isaiah.whitfield@gmail.com>
2021-02-05 08:59:01 -0500

    Extracting some logic from the featured movies component into its own place to make clean up my render method. Creating a new branch to work on this.

--- client/src/frontend/components/movies/featured_movies.jsx
+++ client/src/frontend/components/movies/featured_movies.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import '../../../stylesheets/featured_movies.scss'
 import ContinueWatchingContainer from './continue_watching_container'
+import MovieIcon from './movie_icon'
 
 // Renders the ContinueWatching component and four featured movies that are hardcoded into the state
-// at selections. This was done to avoid having to hit the database separately for each trailer.
+// at selections. This was done to avoid having to hit the database separately for each trailer. 
+
 class FeaturedMovies extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      selectedMovie: '',
+      selectedMovie: 464052,
       selections: [464052, 775996, 508442, 560144]
     }
   }
 
+  // Loops through watch history and selections arrays and fetch movies for each movie Id.
   componentWillMount() {
     const {currentUser: {watch_history}} = this.props
     if (watch_history) {
@@ -25,49 +28,45 @@ class FeaturedMovies extends React.Component{
     })
   }
 
-  randomIndices() {
-    let chosen = [0,0,0,0]
-    chosen.forEach((val, index) => {
-      let posRand = Math.floor(Math.random() * 20)
-      while (chosen.includes(posRand)){
-        posRand = Math.floor(Math.random() * 20)
-      }
-      chosen[index] = posRand
-    })
-    return chosen
-  }
-
   toggleSelectedMovie(movieIndex) {
     this.setState({ selectedMovie: movieIndex} )
   }
 
-  render() {
-    const continueWatching = []
+  isSelectedMovie(movieIndex) {
+    return this.state.selectedMovie === movieIndex
+  }
+
+  // Returns an array of the current user's watch history
+  continueWatching() {
+    let continueWatching = []
     if (this.props.currentUser.watch_history) {
       this.props.currentUser.watch_history.map(movieId => {
         continueWatching.push(this.props.movies[movieId])
       })
     }
-    
-    
+    return continueWatching
+  }
+
+  render() {
+    let continueWatching = this.continueWatching()
     return (
       <>
+        {/* Renders the Featured movie component with one movie selected by default. */}
         <section className='featured_content'>
-          {this.state.selections.map((movieIndex, index) => {
-            if (!this.props.movies.featured) return null
-            
+          {this.state.selections.map((movieIndex) => {
             let movie = this.props.movies[movieIndex]
-            if (!movie.title) debugger
             return (
               <div id={movieIndex} 
-                className={this.state.selectedMovie === movieIndex ? 'large_featured' : 'small_featured'}
+                className={ this.isSelectedMovie(movieIndex) ? 'large_featured' : 'small_featured' }
                 onClick={() => this.toggleSelectedMovie(movieIndex)}>
                 <h1>{movie.title}</h1>
-                {this.state.selectedMovie === movieIndex ? 
-                <iframe width='320' height='240' frameborder='0'
-                  src={`https://www.youtube.com/embed/${movie.video_key}?rel=0&amp;autoplay=1&mute=1&controls=0`}>
-                </iframe> :
-                <img name={movieIndex} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
+                {/* The selected movie is returned as its trailer and all other featured movies are rendered as normal movie icons */}
+                {
+                  this.isSelectedMovie(movieIndex) ? 
+                  <iframe 
+                    src={`https://www.youtube.com/embed/${movie.video_key}?rel=0&amp;autoplay=1&mute=1&controls=0`}>
+                  </iframe> :
+                  <MovieIcon movie={movie}/>
                 }
                 <p>{movie.overview}</p>
               </div>
